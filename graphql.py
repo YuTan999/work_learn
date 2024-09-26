0f61441ee72b6ca22ddc88e4c4a4b8f042f977a2
ProAV <953735433@qq.com>
2024-09-26 18:05:31 +0800

    3.push for pull at home on 26/09/24

---
+++ graphql.py
@@ -0,0 +1,23 @@
+import requests
+
+url = 'http://10.200.8.104/graphql'
+mutation = '''
+
+mutation {
+     login(
+          passwd: "proav101",
+          user: "admin"
+        ) {
+          errorType {
+            code
+            message
+          }
+          user
+          sid
+        }
+    }
+
+'''
+
+response = requests.post(url, json={'query': mutation})
+print(response.json())
