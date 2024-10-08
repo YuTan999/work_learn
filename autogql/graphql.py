e491237a584a22620519243375f77395b651e6dd
ProAV <953735433@qq.com>
2024-10-08 18:21:27 +0800

    4.push for pull at home on 8/10/24

--- autogql/graphql.py
+++ autogql/graphql.py
@@ -2,7 +2,6 @@ import requests
 
 url = 'http://10.200.8.104/graphql'
 mutation = '''
-
 mutation {
      login(
           passwd: "proav101",
@@ -18,6 +17,7 @@ mutation {
     }
 
 '''
+
 
 response = requests.post(url, json={'query': mutation})
 print(response.json())
